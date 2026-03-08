1bac66214c660406b5d7df86373c5d89b1951381
chaima filali <chaimafilali6@gmail.com>
2026-03-08 14:10:07 +0100

    auth jwt + base roles

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,8 +1,12 @@
 using Backend.Data;
 using Backend.Kafka;
+using Backend.Modules.Auth.Services;
 using Backend.Modules.Events.Services;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Backend.Modules.Tasks.Services;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
  
@@ -19,11 +23,34 @@ builder.Services.AddHostedService<OutboxPublisherService>();
 builder.Services.AddScoped<EventProcessorService>();
 builder.Services.AddScoped<TasksService>();
 builder.Services.AddScoped<EventsService>();
+builder.Services.AddScoped<AuthService>();
 
 
 builder.Services.AddControllers();
+var secretKey = builder.Configuration["Jwt:SecretKey"]!;
+var issuer = builder.Configuration["Jwt:Issuer"]!;
+var audience = builder.Configuration["Jwt:Audience"]!;
 
- 
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = issuer,
+        ValidAudience = audience,
+        IssuerSigningKey = new SymmetricSecurityKey(
+                                       Encoding.UTF8.GetBytes(secretKey))
+    };
+});
+
 
 
 
@@ -36,10 +63,11 @@ using (var scope = app.Services.CreateScope())
     db.Database.EnsureCreated();
 }
 
- 
- 
 
- 
+
+
+
+app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
 
--- Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef39e7515316d23132c337c21ae94712cf944687")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8981ae8f39045fd2d5e341e3c39fe9472541faa3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
