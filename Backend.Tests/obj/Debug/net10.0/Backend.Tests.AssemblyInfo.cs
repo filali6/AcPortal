f701b68fafd63cd4a275282012c2b8f966a80ade
chaima filali <chaimafilali6@gmail.com>
2026-09-12 14:12:25 +0100

    change

--- Backend.Tests/obj/Debug/net10.0/Backend.Tests.AssemblyInfo.cs
+++ Backend.Tests/obj/Debug/net10.0/Backend.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1802d780eae945544fa02ab7963fbfc5217d7433")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79fb26da1987274b20069a2302bf38c7335578a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
